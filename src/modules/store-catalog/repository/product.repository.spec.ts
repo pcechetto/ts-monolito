4a02303e036168a1cc5d8221b11a90d43b40a28b
pcechetto <pedrocechetto@hotmail.com>
2024-09-27 12:53:51 -0300

    store-catalog: findAll product repository

---
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -0,0 +1,50 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "./product.model";
+import exp from "constants";
+import ProductRepository from "./product.repository";
+
+describe("product repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should find all products", async () => {
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      salesPrice: 100,
+    });
+
+    await ProductModel.create({
+      id: "2",
+      name: "Product 2",
+      description: "Product 2 description",
+      salesPrice: 200,
+    });
+
+    const productRepository = new ProductRepository();
+    const products = await productRepository.findAll();
+
+    expect(products).toHaveLength(2);
+    expect(products[0].id.id).toBe("1");
+    expect(products[0].name).toBe("Product 1");
+
+    expect(products[1].id.id).toBe("2");
+    expect(products[1].name).toBe("Product 2");
+  });
+});
