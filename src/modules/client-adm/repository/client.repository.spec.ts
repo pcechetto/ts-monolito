cc698d26f6d08a0f40ff0d86b82c3b397c90fb1f
pcechetto <pedrocechetto@hotmail.com>
2024-09-30 15:58:11 -0300

    client repository: add

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "./client.model";
 import ClientRepository from "./client.repository";
+import Client from "../domain/client.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
 
 describe("ClientRepository test", () => {
   let sequelize: Sequelize;
@@ -37,7 +39,33 @@ describe("ClientRepository test", () => {
     expect(result.name).toEqual(client.name);
     expect(result.email).toEqual(client.email);
     expect(result.address).toEqual(client.address);
-    expect(result.createdAt).toBeDefined();
-    expect(result.updatedAt).toBeDefined();
+    expect(result.createdAt).toStrictEqual(client.createdAt);
+    expect(result.updatedAt).toStrictEqual(client.updatedAt);
+  });
+
+  it("should create a client", async () => {
+    const client = new Client({
+      id: new Id("1"),
+      name: "Client 1",
+      email: "qYkT8@example.com",
+      address: "Client 1 address",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const clientRepository = new ClientRepository();
+    await clientRepository.add(client);
+
+    const result = await ClientModel.findOne({
+      where: { id: "1" },
+    });
+
+    expect(result).toBeDefined();
+    expect(result.id).toEqual(client.id.id);
+    expect(result.name).toEqual(client.name);
+    expect(result.email).toEqual(client.email);
+    expect(result.address).toEqual(client.address);
+    expect(result.createdAt).toEqual(client.createdAt);
+    expect(result.updatedAt).toStrictEqual(client.updatedAt);
   });
 });
--- src/modules/client-adm/repository/client.repository.ts
+++ src/modules/client-adm/repository/client.repository.ts
@@ -4,8 +4,15 @@ import CLientGateway from "../gateway/client.gateway";
 import { ClientModel } from "./client.model";
 
 export default class ClientRepository implements CLientGateway {
-  add(client: Client): Promise<void> {
-    throw new Error("Method not implemented.");
+  async add(client: Client): Promise<void> {
+    await ClientModel.create({
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
   }
   async find(id: string): Promise<Client> {
     const client = await ClientModel.findOne({ where: { id } });
